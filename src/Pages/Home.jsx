3114f18e009e6cd4c684823104b61b6161c0308d
K-Siim <kevinsiim007@gmail.com>
2025-08-20 21:22:03 +0300

    Page without contact jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,11 @@
 import Hero from "../Components/Home/Hero";
 import Cards from "../Components/Home/Cards";
-import Contact from "../Components/Home/Contact";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Cards />
-      <Contact/>
     </div>
   );
 };
